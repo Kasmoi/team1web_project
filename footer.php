d1c00d74772ed111ea3cc316d79512565ca45889
Kasmoi <35563413+Kasmoi@users.noreply.github.com>
2022-02-13 20:59:08 +0200

    added footer/style. uploaded most of the pics

---
+++ footer.php
@@ -0,0 +1,57 @@
+<link rel="stylesheet" href="css/nav-footer.css">
+<footer>
+  <!--Footer main-->
+  <section class="footer mtb-10">
+  <div class="container">
+    <div class="ft-main pdtb-2-4">
+      <div class="ft-main-item">
+          <img src="uploads/logo/footer-logo.png" alt="logo" height="61">
+          <h3 class="ft-brand">Gym One</h3>
+          <ul>
+            <li><i class="fa fa-map-marker" aria-hidden="true"></i> Visamaentie 13100 Hameelinna</li>
+            <li><i class="fas fa-phone-alt"></i> 040 -525222</li>
+          </ul>
+        </div>
+        <div class="ft-main-item">
+          <h3 class="ft-scl-title">Follow us</h3>
+          <ul class="ft-social-list">
+            <li><a href="#"><i class="fab fa-facebook fa-2x"></i></a></li>
+            <li><a href="#"><i class="fab fa-twitter fa-2x"></i></a></li>
+            <li><a href="#"><i class="fab fa-instagram fa-2x"></i></a></li>
+            <li><a href="#"><i class="fab fa-linkedin fa-2x"></i></a></li>
+            <li><a href="#"><i class="fab fa-youtube fa-2x"></i></a></li>
+          </ul>
+        </div>
+      <div class="ft-main-item">
+        <h3 class="ft-link-title">Quick Links</h3>
+        <ul>
+        <li><a href="membership.php">Membership</a></li>
+        <li><a href="trainers.php">Trainers</a></li>
+        <li><a href="contact.php">Contact Us</a></li>
+      </ul>
+    </div>
+  </div>
+    <p class="pd-2-4">Opening Hours: SUN-SAT  4:00-21:00</p>
+
+  </div>
+
+</section>
+   <!-- Footer legal -->
+  <div class="ft-legal">
+    <div class="container">
+      <p>&copy; 2022 Gym-One Designed by Team One.</p>
+     </div>
+  </div>
+
+
+</footer>
+
+
+
+<script src="js/nav.js">
+</script>
+
+<script src="js/slider.js"></script>
+
+</body>
+</html>
