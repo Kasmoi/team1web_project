68db57ce969d01a0d9e321d72a0fff1c5021106d
Danila Karpov <gpdanila@gmail.com>
2022-02-15 04:06:44 +0200

    Add header

---
+++ header.php
@@ -0,0 +1,35 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link href="./css/header.css" rel="stylesheet">
+<header>
+      <div class="nav-wrapper">
+        <div class="grad-bar"></div>
+        <div class="container">
+          <nav class="navbar">
+            <a class="nav-brand" href="#"><img src="uploads/logo/footer-logo.png" alt="logo image"></a>
+            <div class="menu-toggle" id="mobile-menu">
+              <span class="bar"></span>
+              <span class="bar"></span>
+              <span class="bar"></span>
+
+            </div>
+            <ul class="nav">
+              <li class="nav-item">
+                <a href = "index.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a href = "membership.html">Membership</a>
+              </li>
+              <li class="nav-item">
+                <a href = "trainers.html">Trainers</a>
+              </li>
+              <li class="nav-item">
+                <a href = "contact.html">Contact Us</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+      </div>
+
+
+    </header>
