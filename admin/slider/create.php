9ffe221b655556f69889c95074d31a9e3f1b3226
Deepan Gautam <dpngtm@gmail.com>
2022-02-17 19:34:33 +0200

    changes to some admin dashboard

---
+++ admin/slider/create.php
@@ -0,0 +1,14 @@
+<?php 
+    $title = "Add Slider";
+    require("../inc/head.php");
+    require("../inc/header.php");
+    require("../inc/sidebar.php");
+ ?>
+
+<main>
+    
+</main>
+<?php require("../inc/footer.php");?>
+<script type="text/javascript" src="../assets/js/sidebar.js"></script>
+</body>
+</html>
