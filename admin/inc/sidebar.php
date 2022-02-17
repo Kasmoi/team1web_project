252f4a942c5f211913826bb33d7b0014a9612b92
Deepan Gautam <dpngtm@gmail.com>
2022-02-17 19:34:55 +0200

    changes to admindashboard

--- admin/inc/sidebar.php
+++ admin/inc/sidebar.php
@@ -1,53 +1,12 @@
 <aside class="aside">
-    <div class="aside_close-icon">
-        <strong>&times;</strong>
-    </div>
-    <div class="aside_menu">
-        <div class="aside_menu-item admin-info"><i class="fa-solid fa-user"></i> Admin </div>
-        <div class="aside_menu-item dropdown">
-            <button class="dropbtn">Blogs
-            <i class="fa fa-caret-down"></i>
-            </button>
-            <div id ="dropdown-menu" class="dropdown-content">
-            <a href="#">Create</a>
-            <a href="#">View</a>
-            </div>   
+        <div class="aside_close-icon">
+          <strong>&times;</strong>
         </div>
-        <div class="aside_menu-item dropdown">
-            <button class="dropbtn">Membership
-            <i class="fa fa-caret-down"></i>
-            </button>
-            <div id ="dropdown-menu" class="dropdown-content">
-            <a href="#">Create</a>
-            <a href="#">View</a>
-            </div>   
-        </div>
-        <div class="aside_menu-item dropdown">
-            <button class="dropbtn">Trainers
-            <i class="fa fa-caret-down"></i>
-            </button>
-            <div id ="dropdown-menu" class="dropdown-content">
-            <a href="#">Create</a>
-            <a href="#">View</a>
-            </div>   
-        </div>
-        <div class="aside_menu-item dropdown">
-            <button class="dropbtn">GalleryImages
-            <i class="fa fa-caret-down"></i>
-            </button>
-            <div id ="dropdown-menu" class="dropdown-content">
-            <a href="#">Create</a>
-            <a href="#">View</a>
-            </div>   
-        </div>
-        <div class="aside_menu-item dropdown">
-            <button class="dropbtn">Sliders
-            <i class="fa fa-caret-down"></i>
-            </button>
-            <div id ="dropdown-menu" class="dropdown-content">
-            <a href="#">Create</a>
-            <a href="#">View</a>
-            </div>   
-        </div>
-    </div>
-</aside>
+        <ul class="aside_list">
+          <li class="aside_list-item"><a href=""><i class="fa-solid fa-book"></i> Blogs</a></li>
+          <li class="aside_list-item"><a href=""><i class="fa-solid fa-users"></i> Memberships</a></li>
+          <li class="aside_list-item"><a href=""><i class="fa-solid fa-dumbbell"></i> Trainers</a></li>
+          <li class="aside_list-item"><a href=""><i class="fa-solid fa-images"></i> Gallery</a></li>
+          <li class="aside_list-item"><a href="/team1web_project/admin/slider/create.php"><i class="fa-solid fa-sliders"></i> Sliders</li></a>
+        </ul>
+      </aside>
